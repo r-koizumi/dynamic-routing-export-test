8c1af32af8bb39e6c868c02a1576d93ecaf1fe88
r-koizumi <r-koizumi@mi-labo.co.jp>
2021-02-01 16:25:23 +0900

    view page by URL at reload page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Header from '../components/header'
+import { useRouter } from "next/router";
 
-const Home = () => (
-  <>
-    <Header />
-    <h1>Hello World!</h1>
-  </>
-)
+const Home = () => {
+  const router = useRouter();
+  console.log(router);
+  if (router.asPath !== "/") {
+    router.push(router.asPath);
+  }
+  return (
+    <>
+      <Header />
+      <h1>Hello World!</h1>
+    </>
+  );
+}
 
 export default Home
