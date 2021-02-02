ae6d3dc71dad3e8b4591a632d53e3bc754f47249
r-koizumi <r-koizumi@mi-labo.co.jp>
2021-02-02 23:22:06 +0900

    map route to dynamic routing pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 import Header from '../components/header'
-import { useRouter } from "next/router";
 
 const Home = () => {
-  const router = useRouter();
-  console.log(router);
-  if (router.asPath !== "/") {
-    router.push(router.asPath);
-  }
   return (
     <>
       <Header />
